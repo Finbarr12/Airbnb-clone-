66799575b5113ba2eeaff3fbad37d3af1a7e4e02
finbarr12 <ogbonnafinbarr@gmail.com>
2024-01-10 15:17:39 +0100

    added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,18 +2,25 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import Colors from "../../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const _layout = () => {
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: {
+          fontFamily: "mont",
+        },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           tabBarLabel: "Explore",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
