b94a50d304534c0048f8547ecba691892299cc0b
finbarr12 <ogbonnafinbarr@gmail.com>
2024-01-10 14:58:43 +0100

    added

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const index = () => {
+const explore = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>explore</Text>
     </View>
   );
 };
 
-export default index;
+export default explore;
