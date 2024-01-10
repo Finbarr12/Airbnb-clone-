a0d1afbb428eb48aee6c128a5a9039333882ac6c
finbarr12 <ogbonnafinbarr@gmail.com>
2024-01-10 15:28:48 +0100

    added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,12 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import Colors from "../../constants/Colors";
-import { Ionicons } from "@expo/vector-icons";
+import {
+  FontAwesome,
+  FontAwesome5,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 const _layout = () => {
   return (
@@ -20,6 +25,46 @@ const _layout = () => {
           tabBarLabel: "Explore",
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="wishlists"
+        options={{
+          tabBarLabel: "Wishlists",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="trips"
+        options={{
+          tabBarLabel: "Trips",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="airbnb" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          tabBarLabel: "Inbox",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="message-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" color={color} size={size} />
           ),
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Link href={"/(modals)/login"}>Login</Link>
+      <Link href={"/(modals)/booking"}>Bookings</Link>
     </View>
   );
 };
