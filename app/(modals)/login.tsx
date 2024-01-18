4f32f7275db0cff27cd4a4c35c38643c735130b8
finbarr12 <ogbonnafinbarr@gmail.com>
2024-01-18 11:04:01 +0100

    added

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import React from "react";
 
 const login = () => {
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <TextInput style={styles.input} placeholder="Input Name ...." />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  input: {
+    width: "100%",
+    height: 35,
+    borderRadius: 4,
+    borderStyle: "solid",
+    borderColor: "red",
+    borderWidth: 1,
+    paddingLeft: 10,
+  },
+});
 
 export default login;
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const profile = () => {
   return (
     <View>
-      <Text>profile</Text>
+      <Link href={"/(modals)/login"}>
+        <Text>Login</Text>
+      </Link>
     </View>
   );
 };
