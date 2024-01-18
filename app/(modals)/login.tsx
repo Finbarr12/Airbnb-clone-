b42b81604d1168f671059092747f50423ff0ce29
finbarr12 <ogbonnafinbarr@gmail.com>
2024-01-18 11:24:31 +0100

    added

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,10 +1,24 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 
 const login = () => {
   return (
     <View style={styles.container}>
       <TextInput style={styles.input} placeholder="Input Name ...." />
+      <TextInput style={styles.input} placeholder="Input Email ...." />
+      <TextInput style={styles.input} placeholder="Input Password ...." />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Login</Text>
+      </TouchableOpacity>
+      <Text style={{ marginTop: 20 }}>
+        Already have an account? <Text style={{ color: "blue" }}>Sign in</Text>
+      </Text>
     </View>
   );
 };
@@ -16,12 +30,27 @@ const styles = StyleSheet.create({
   },
   input: {
     width: "100%",
-    height: 35,
+    height: 40,
     borderRadius: 4,
     borderStyle: "solid",
     borderColor: "red",
     borderWidth: 1,
     paddingLeft: 10,
+    marginBottom: 30,
+  },
+  button: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "red",
+    color: "white",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
