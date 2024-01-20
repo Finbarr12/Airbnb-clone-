56f1c5882fe8f07c95acd3fd5be696c18ba80812
finbarr12 <ogbonnafinbarr@gmail.com>
2024-01-20 03:43:07 +0100

    added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,13 +3,7 @@ import React from "react";
 import { Link } from "expo-router";
 
 const index = () => {
-  return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Bookings</Link>
-      <Link href={"/listing/123"}>Listing details</Link>
-    </View>
-  );
+  return <View></View>;
 };
 
 export default index;
