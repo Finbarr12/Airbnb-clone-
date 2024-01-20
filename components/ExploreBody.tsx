1ef57621532c856c8facf2106304593e1303b357
finbarr12 <ogbonnafinbarr@gmail.com>
2024-01-20 04:50:43 +0100

    addded

--- components/ExploreBody.tsx
+++ components/ExploreBody.tsx
@@ -1,55 +1,19 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import React from "react";
 import { Entypo } from "@expo/vector-icons";
+import HousesBooked from "./HousesBooked";
 
 const ExploreBody = () => {
   return (
     <View style={{ padding: 20 }}>
-      <View style={styles.imagehold}>
-        <Image
-          style={styles.img}
-          source={require("../assets/images/house.jpg")}
-        />
-      </View>
-      <View style={styles.hotelhold}>
-        <View>
-          <Text style={{ fontWeight: "800", fontSize: 18 }}>
-            Telgte,Germany
-          </Text>
-          <Text style={{ fontSize: 14, color: "#c9c7c7" }}>
-            Individual Host
-          </Text>
-          <Text style={{ fontSize: 14, color: "#c9c7c7" }}>24-29 Nov</Text>
-          <Text>$400.00 night</Text>
-        </View>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Entypo name="star" size={17} color="black" />
-          <Text>4.95</Text>
-        </View>
-      </View>
+      <ScrollView>
+        <HousesBooked />
+        <HousesBooked />
+        <HousesBooked />
+        <HousesBooked />
+      </ScrollView>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  imagehold: {
-    width: "100%",
-    height: 400,
-    backgroundColor: "red",
-    borderRadius: 20,
-    overflow: "hidden",
-  },
-  img: {
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-  },
-  hotelhold: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    marginTop: 17,
-  },
-});
 
 export default ExploreBody;
---
+++ components/HousesBooked.tsx
@@ -0,0 +1,57 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import { Entypo } from "@expo/vector-icons";
+
+const HousesBooked = () => {
+  return (
+    <View>
+      <View style={styles.imagehold}>
+        <Image
+          style={styles.img}
+          source={require("../assets/images/house.jpg")}
+        />
+      </View>
+      <View style={styles.hotelhold}>
+        <View>
+          <Text style={{ fontWeight: "800", fontSize: 18 }}>
+            Telgte,Germany
+          </Text>
+          <Text style={{ fontSize: 14, color: "#c9c7c7" }}>
+            Individual Host
+          </Text>
+          <Text style={{ fontSize: 14, color: "#c9c7c7" }}>24-29 Nov</Text>
+          <Text style={{ marginTop: 10, fontWeight: "900", fontSize: 14 }}>
+            $400.00 night
+          </Text>
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Entypo name="star" size={17} color="black" />
+          <Text>4.95</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imagehold: {
+    width: "100%",
+    height: 400,
+    backgroundColor: "red",
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  },
+  hotelhold: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    marginTop: 17,
+  },
+});
+
+export default HousesBooked;
